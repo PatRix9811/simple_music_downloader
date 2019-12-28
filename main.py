57216ea0fc42028972aaf464f2a57ecefc0a4b9c
Patryk Wójtowicz <patrix9811@gmail.com>
2019-12-28 17:54:27 +0100

    added finding files and if file was not found they was added into filed.txt file

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-from selenium import webdriver
+#from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup, SoupStrainer
+from pprint import pprint as pp
 
 def load_title_list():
 	with open('titles.txt', 'r') as file:
 		return [item.strip() for item in file.readlines()]
 
 
+def find_track(track_title):
+	res = requests.get('http://ulub.pl/szukaj.html', params='q=%s' % track_title)
+	only_a_tag = SoupStrainer('a', title = track_title)
+	soup = BeautifulSoup(res.text, 'html.parser',parse_only = only_a_tag)
+	if soup.a == None:
+		with open('filed.txt','a') as filed:
+			filed.write(track_title)
+			return None
+	else:
+		return soup.a['href']
+
+
+
 def main():
-	driver = webdriver.Firefox()
-	driver.get('http://ulub.pl/')
-	if "Wyszukiwarka mp3" not in driver.title:
-		print("Open wrong site. Please check url adress")
-		driver.close()
-		exit()
-
 	titles = load_title_list()
-
-	driver.close()
+	for title in titles:
+		find_track(title)
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -2,10 +2,17 @@ import main
 import unittest
 
 class SimpleTest(unittest.TestCase):
-	def test_one(self):
+	def test_load_list(self):
 		result = main.load_title_list()
 		self.assertIsInstance(result, list)
 
+	def test_find_track1(self):
+		result = main.find_track('Chada, Bezczel, ZBUKU ft. KaeN - Kontrabanda: To nie nasz klimat')
+		self.assertIsInstance(result, str)
+
+	def test_find_track2(self):
+		result = main.find_track('dsfgsdfghfdsgsdfhasdg')
+		self.assertEqual(result, None)
 
 if __name__ == '__main__':
 	unittest.main()
