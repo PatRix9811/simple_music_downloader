2c085d7eb59d4a2ab72367ca5a5c268e5612d61b
Patryk Wójtowicz <patrix9811@gmail.com>
2020-01-04 12:29:38 +0100

    Adding download and get_requets functions

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
-#from selenium import webdriver
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
-from pprint import pprint as pp
+from selenium import webdriver
+
+def get_request(url,filters= None,is_param=False,**kwargs):
+	if is_param:
+		res = requests.get(url, params='q=%s' % kwargs['title'])
+	else:
+		res = requests.get(url, allow_redirects = True)
+
+	if filters != None:
+		only_a_tag = SoupStrainer(filters, title = kwargs['title'])
+		return BeautifulSoup(res.text, 'html.parser',parse_only = only_a_tag)
+	else:
+		return res
+		
+
+def download(url):
+	print('Downloading...')
+	profile = webdriver.FirefoxProfile()
+	profile.set_preference("browser.helperApps.neverAsk.saveToDisk",'audio/mpeg')
+	driver = webdriver.Firefox(profile)
+	driver.get(f'http://ulub.pl/{url}')
+	button = driver.find_element_by_class_name('rodo_go_a')
+	button.click()
+	button = driver.find_element_by_class_name('pobierz')
+	button.click()	
+
 
 def load_title_list():
+	print('Loadin title list...')
 	with open('titles.txt', 'r') as file:
 		return [item.strip() for item in file.readlines()]
 
 
 def find_track(track_title):
-	res = requests.get('http://ulub.pl/szukaj.html', params='q=%s' % track_title)
-	only_a_tag = SoupStrainer('a', title = track_title)
-	soup = BeautifulSoup(res.text, 'html.parser',parse_only = only_a_tag)
+	print(f'Searching {track_title}')
+	soup = get_request('http://ulub.pl/szukaj.html',filters ='a', is_param=True, title = track_title)
+	
 	if soup.a == None:
 		with open('filed.txt','a') as filed:
-			filed.write(track_title)
+			filed.write(track_title+'\n')
 			return None
 	else:
-		return soup.a['href']
+		download(soup.a['href'])
 
 
 
